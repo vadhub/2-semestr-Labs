c5b341e275931335e0f54cc7b93a29fc28658f3c
Vadim <gabderahmanov99@gmail.com>
2019-02-24 14:48:15 +0400

    first commite

---
+++ Lab#1/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "enter the num a column and row:\n";
+    int numArray = 0;
+
+    int n = 0, i,j,kx = 0,ky = 0;
+
+    float sum = 0, res = 0;
+    int formul = 0;
+    cin >> numArray;
+
+    cout << "enter the n\n";
+
+    cin >> n;
+
+    formul = 2*n + 1;
+    float **arrayA = new float *[numArray];
+    float **arrayB = new float *[formul];
+    for (i = 1; i<=numArray;i++){
+        arrayA[i] = new float[numArray];
+        arrayB[i] = new float[formul];
+    }
+    cout << "enter the numbers:\n";
+    for(i = 1;i<=numArray;i++){
+        for(j = 1;j<=numArray;j++){
+            cout << "array[" << i << "][" << j <<"]: ";
+            cin >> arrayA[i][j];
+        }
+    }
+
+    //algot
+
+    for(i = 1;i<=numArray;i++){
+        for(j = 1;j<=numArray;j++){
+                while(res!=0){
+                    res = 4;
+
+                }
+        }
+    }
+    cout <<sum <<"\n";
+
+     for(i = 1;i<=numArray;i++){
+        for(j =1;j<=numArray;j++){
+            cout << arrayA[i][j];
+
+            cout << "\t";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+
