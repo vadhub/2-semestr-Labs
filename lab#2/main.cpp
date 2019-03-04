8e7619ea4ca3eec37676f331a8647ee2a13e1fb6
Vadim <gabderahmanov99@gmail.com>
2019-03-04 09:50:17 +0400

    end

--- lab#2/main.cpp
+++ lab#2/main.cpp
@@ -6,34 +6,38 @@ using namespace std;
 
 int main()
 {
-    string str = ")(";
+    string str = "({)}";
     stack<char> stk;
 
     stk.push(str[0]);
 
     for(int i = 1;i<str.length();i++){
-        if(str[i] == '('){
+        if(str[i] == '(' || str[i] == '{' || str[i] == '[' || str[i] == '<'){
             stk.push(str[i]);
         }else if(str[i] == ')'){
-           stk.pop();
-        }
-
-         if(str[i] == '<'){
-            stk.push(str[i]);
-        }else if(str[i] == '>'){
-           stk.pop();
-        }
-
-         if(str[i] == '{'){
-            stk.push(str[i]);
-        }else if(str[i] == '}'){
-           stk.pop();
-        }
-
-         if(str[i] == '['){
-            stk.push(str[i]);
+            if(stk.top()=='('){
+                stk.pop();
+            }else{
+                break;
+            }
         }else if(str[i] == ']'){
-           stk.pop();
+            if(stk.top()=='['){
+                stk.pop();
+            }else{
+                break;
+            }
+        }else if(str[i] == '{'){
+            if(stk.top()=='}'){
+                stk.pop();
+            }else{
+                break;
+            }
+        }else if(str[i] == '<'){
+            if(stk.top()=='>'){
+                stk.pop();
+            }else{
+                break;
+            }
         }
     }
 
