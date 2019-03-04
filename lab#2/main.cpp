5e1f95f73cf5d6cb8153cf6b5036035df16a9467
Vadim <gabderahmanov99@gmail.com>
2019-03-04 09:42:34 +0400

    stack

--- lab#2/main.cpp
+++ lab#2/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <stack>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str = ")(";
+    stack<char> stk;
+
+    stk.push(str[0]);
+
+    for(int i = 1;i<str.length();i++){
+        if(str[i] == '('){
+            stk.push(str[i]);
+        }else if(str[i] == ')'){
+           stk.pop();
+        }
+
+         if(str[i] == '<'){
+            stk.push(str[i]);
+        }else if(str[i] == '>'){
+           stk.pop();
+        }
+
+         if(str[i] == '{'){
+            stk.push(str[i]);
+        }else if(str[i] == '}'){
+           stk.pop();
+        }
+
+         if(str[i] == '['){
+            stk.push(str[i]);
+        }else if(str[i] == ']'){
+           stk.pop();
+        }
+    }
+
+    if(stk.empty()){
+        cout<<"bracket all right";
+    }else{
+        cout<<"bracket no right";
+    }
     return 0;
 }
