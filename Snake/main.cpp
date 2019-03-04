d76ad85f4121deb402e6e763c29e2fcbb37f7976
Vadim <gabderahmanov99@gmail.com>
2019-03-04 19:45:25 +0400

    add conditions in snake

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -8,22 +8,22 @@ using namespace std;
 bool gameOver;
 const int width = 20;
 const int height = 20;
-enum eNum {stop, left_, up_, right_, down_};
-eNum switching;
+enum dir {stop = 0, left_, up_, right_, down_};
+dir e;
 
-int x, y, dotX, dotY, score;
+int x = 0, y = 0, dotX, dotY, score;
 
 
 void setup(){
     gameOver = false;
 
-    switching = stop;
+    e = stop;
 
-    x = width / 2;
-    y = height / 2;
+    x = width / 2 - 1;
+    y = height / 2 - 1;
 
-    dotX = rand()%width-1;
-    dotY = rand()%height-1;
+    dotX = rand()%width;
+    dotY = rand()%height;
 
     score = 0;
 }
@@ -41,13 +41,13 @@ void draw(){
                 cout<<"#";
             }
 
-            if(i==y&&j==x){
-                cout<<"O";
-            }else if(i == dotX && j == dotY){
-                cout<<"@";
-            }else{
-                cout<<" ";
-            }
+                                    if(i == y&&j == x){
+                                        cout<<"O";
+                                    }else if(i == dotX && j == dotY){
+                                        cout<<"@";
+                                    }else{
+                                        cout<<" ";
+                                    }
         }
         cout << endl;
     }
@@ -56,6 +56,7 @@ void draw(){
         cout<<"#";
     }
     cout<<endl;
+    cout << "Score: "<< score<< endl;
 
 }
 
@@ -63,18 +64,18 @@ void inKey(){
     if(_kbhit()){
         switch(_getch()){
             case 'a':
-                switching = left_;
+                e = left_;
                 break;
             case 'd':
-                switching = right_;
+                e = right_;
                 break;
             case 'w':
-                switching = up_;
+                e = up_;
                 break;
             case 's':
-                switching = down_;
+                e = down_;
                 break;
-            case 't':
+            case 'x':
                 gameOver = true;
                 break;
         }
@@ -82,20 +83,36 @@ void inKey(){
 }
 
 void gamePlay(){
-    switch(switching){
+    switch(e){
         case left_:
             x--;
             break;
-        case up_:
-            y--;
-            break;
         case right_:
             x++;
+            break;
+        case up_:
+            y--;
             break;
         case down_:
             y++;
             break;
     }
+
+                                        if(x > width-2)
+                                            x = 0;
+                                        if(x < 0)
+                                            x = width-2;
+                                        if(y > height-1)
+                                            y = 0;
+                                        if(y < 0)
+                                            y = height-1;
+
+    if(x == dotX && y == dotY){
+        score += 20;
+        dotX = rand() % width;
+        dotY = rand() % height;
+    }
+
 }
 
 
@@ -108,6 +125,5 @@ int main()
         draw();
         Sleep(250);
     }
-    cout<<left;
     return 0;
 }
