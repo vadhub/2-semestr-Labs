a51d5c02ca73b505bd8d90174cd3985f2987a36f
Vadim <gabderahmanov99@gmail.com>
2019-03-19 09:52:22 +0400

    add construct; construct copy; destruct

--- lab#3/main.cpp
+++ lab#3/main.cpp
@@ -2,77 +2,80 @@
 #include <fstream>
 #include <string.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Product{
+       private: int countProduct;
        private: string nameProduct;
-       private: int countProduct = 10;
-       private: int priceProduct;
+       private: double priceProduct;
        private: int countSoldProduct;
-       private: string products[30];
 
-       //constructors on all happen life
+       public: Product(int c, string name, double p, int sl){
+           countProduct = c;
+           nameProduct = name;
+           priceProduct = p;
+           countSoldProduct = sl;
+       };
 
-        public: Product(string nameprod, int countprod, int priceprod, int countSoldProd){
-            nameProduct = nameprod;
-            countProduct = countprod;
-            priceProduct = priceprod;
-            countSoldProduct = countSoldProd;
-        };
+        public: Product(int c, string name, double p){
+           countProduct = c;
+           nameProduct = name;
+           priceProduct = p;
+       };
 
-        public: Product(string nameprod, int countprod, int priceprod){
-            nameProduct = nameprod;
-            countProduct = countprod;
-            priceProduct = priceprod;
-        };
+        public: Product(int c, string name){
+           countProduct = c;
+           nameProduct = name;
+       };
 
-        public: Product(string nameprod, int countprod){
-            nameProduct = nameprod;
-            countProduct = countprod;
-        };
+       //copy constructor
+       public: Pruduct(Product & obj){
 
-        public: Product(string nameprod){
-            nameProduct = nameprod;
-        };
+       }
 
-        public: Product(int countprod){
-            countProduct = countprod;
-        };
+       //destruct
+       ~public: Pruduct(){
 
-        public: int PayProduct(){
-            print("You bought "+ nameProduct + ": ");
-            return countSoldProduct++;
+       }
+
+        public: Product(string name){
+           nameProduct = name;
+       };
+
+        public: Product(int c){
+           countProduct = c;
+       };
+
+        public: Product(){};
+
+        public: void buy(){
+            countProduct--;
+            countSoldProduct++;
         }
 
-        public: int summPrice(){
-            return countSoldProduct*priceProduct;
+         public: void pay(){
+            countProduct++;
+            countSoldProduct--;
         }
 
-        public: void Bought(){
-            int i = 0;
-            products[i] = nameProduct;
-            i++;
+        public: void features(){
+            cout<<"* "<<nameProduct<<" * "<<priceProduct<<" * "<<countProduct;
         }
 
-        /*private: string convector(int intor){
-                std::string s = std::to_string(intor);
-                return s;
-        }*/
+        private: int price(){
+            if(priceProduct>10&&countProduct>20){
+               priceProduct = priceProduct * countProduct;
+            }else{
+                priceProduct = (priceProduct * countProduct)/4;
+            }
 
-        private: void table(){
-            print("************************************");
-                std::string stRpriceProduct = std::to_string(priceProduct);
-                std::string stRCountProduct = std::to_string(countProduct);
-            for(int i = 0;i<countProduct;i++){
-                    std::string strI = std::to_string(i);
-                print("* " + strI + " * "+ nameProduct + " * " + stRPriceProduct + " * "+stRCountProduct+" *");
+            if(countProduct>300&&priceProduct>100){
+                priceProduct = priceProduct/2;
             }
         }
 
-        private: void print(string str){
-            cout << str;
-        }
 
 };
 
