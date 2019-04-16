f4f175913d2c3872563155e64a6001c09233d623
Vadim <gabderahmanov99@gmail.com>
2019-04-16 13:58:00 +0400

    end lab

--- lab#5/main.cpp
+++ lab#5/main.cpp
@@ -20,20 +20,20 @@ class PhysicalBody{
         }
 
         double getA(){
-            return v;
+            return a;
         }
 
 
-        void setV(double sA){
+        void setA(double sA){
             a = sA;
         }
 
         double getV0(){
-            return v;
+            return v0;
         }
 
 
-        void setV(double sV0){
+        void setV0(double sV0){
            v0 = sV0;
         }
 
@@ -46,11 +46,11 @@ class PhysicalBody{
         PhysicalBody(double vs, double as, double v0s){
             v = vs;
             a = as;
-            v0a = v0;
+            v0s = v0;
         }
 
-        double timeAcc(double vs, double as){
-            return vs/as;
+        double timeAcc(double v0s, double as){
+            return v0s/as;
         }
 
         double timeAcc(double v0s, double as, double vs){
@@ -62,5 +62,14 @@ class PhysicalBody{
 
 int main()
 {
+    double v = 10.2;
+    double a = 1.1;
+    double v0 = 10;
+
+    double time;
+
+    PhysicalBody pb = PhysicalBody(v, a, v0);
+    time = pb.timeAcc(v0, a);
+    std::cout<<"Time acceleration"<<time<<std::endl;
     return 0;
 }
