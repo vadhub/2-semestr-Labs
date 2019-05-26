1434609e745adcad670f9a72c9e1dc1e2a19595c
Vadim <gabderahmanov99@gmail.com>
2019-05-26 14:22:12 +0400

    add overload operator -

--- lab#7/main.cpp
+++ lab#7/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
+#include <vector>
+
+#include <iterator>
+
 
 using namespace std;
 
@@ -40,9 +44,16 @@ class Set{
         Set<T> operator -(Set<T> s){
 
             Set result;
+            vector<T> vec1(a.begin(), a.end());
+            vector<T> vec2(s.a.begin(), s.a.end());
+            vector<T> v;
 
-            sort(a.begin(), a.end());
-            sort(s.a.begin(), s.a.end());
+            sort(vec1.begin(), vec1.end());
+            sort(vec2.begin(), vec2.end());
+
+            set_intersection(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), back_inserter(v));
+
+            result.a.insert(v.begin(), v.end());
 
             return result;
         }
@@ -53,24 +64,30 @@ int main()
 {
     set<int> s;
     set<int> s2;
-    set<int> s3;
 
     s.insert(1);
     s.insert(2);
+    s.insert(3);
 
     s2.insert(2);
     s2.insert(3);
     s2.insert(4);
 
-    s3.insert(s.begin(), s.end());
-    s3.insert(s2.begin(), s2.end());
-
     Set<int> a(s);
     Set<int> b(s2);
 
     Set<int> c = a + b;
+    Set<int> d = a - b;
+
+    cout<<d.getSet().size()<<endl;
 
     for(set<int>::iterator it = c.getSet().begin(); it!=c.getSet().end(); ++it){
+        cout<<(*it)<<endl;
+    }
+
+    cout<<endl;
+
+    for(set<int>::iterator it = d.getSet().begin(); it!=d.getSet().end(); ++it){
         cout<<(*it)<<endl;
     }
 
